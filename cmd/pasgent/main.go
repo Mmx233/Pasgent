890b9536cfcddb7cad13a56daf5fe0884b704d9c
Mmx <green.dusk8264@fastmail.com>
2024-12-23 00:48:13 +0800

    improve: set nested log formatter

--- cmd/pasgent/main.go
+++ cmd/pasgent/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/Mmx233/Pasgent/pageant"
+	nested "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	logger := logrus.StandardLogger()
-	logger.Infoln("Pasgent started")
+	logger.SetFormatter(&nested.Formatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	})
+
+	logger.Infoln("pasgent started")
 	_ = pageant.Run(logger)
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
 	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/sys v0.28.0
 )
